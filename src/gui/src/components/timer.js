ce5704a7638954443c394ca23365002c7754342d
MattToast <matthew.drozt@gmail.com>
2020-09-23 18:38:26 -0400

    Padds number of digits on time

--- src/gui/src/components/timer.js
+++ src/gui/src/components/timer.js
@@ -14,21 +14,29 @@ class Timer extends React.Component {
    }
 
    tick() {
+      const pad_zeros = (number) => {
+         number = number + "";
+         if (number < 10) {
+            number = "0" + number;
+         }
+         return number
+      }
       let now_time = new Date().getTime();
       let time_diff = now_time - this.start_time;
       this.milliseconds = time_diff % 1000;
+      this.milliseconds = Math.floor(this.milliseconds / 100);
       this.seconds = Math.floor(time_diff / 1000);
       this.minutes = Math.floor(this.seconds / 60);
       this.seconds = this.seconds % 60;
       this.hours = Math.floor(this.minutes / 60);
       this.minutes = this.minutes % 60;
       this.setState(state => ({
-         time: `${this.hours}:${this.minutes}:${this.seconds}:${this.milliseconds}`
+         time: `${pad_zeros(this.hours)}:${pad_zeros(this.minutes)}:${pad_zeros(this.seconds)}:${this.milliseconds}`
       }));
    }
 
    componentDidMount() {
-      this.interval = setInterval(() => this.tick(), 100);
+      this.interval = setInterval(() => this.tick(), 10);
    }
 
    componentWillUnmount() {
