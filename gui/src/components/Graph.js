335ab4a3137e21ac72ecd770ced12b6bbe793c2c
MattToast <matthew.drozt@gmail.com>
2020-11-11 18:32:19 -0500

    Makes a simple line graph and fills it with toy data

--- gui/src/components/Graph.js
+++ gui/src/components/Graph.js
@@ -10,14 +10,52 @@ class Graph extends React.Component {
   }
 
   componentDidMount() {
+    // Make random data for now
+    const NUM_POINTS = 50;
+    let rng_data = [];
+    for (let i = 0; i < NUM_POINTS; i++) {
+      rng_data.push({
+        "x": Math.random(),
+        "y": Math.random()
+      });
+    }
+    rng_data.sort(function(first, second) {
+      return first.x - second.x
+    });
+
     const chart = this.canvasRef.current.getContext("2d");
-    new Chart(chart, {});
+    new Chart(chart, {
+      // The type of chart we want to create
+      type: 'line',
+      data: {
+        datasets: [{
+          label: 'Graph',
+          borderColor: 'rgb(0, 0, 0)',
+          data: rng_data
+        }]
+      },
+
+      // Configuration options go here
+      options: {
+        elements: {
+          line: {
+            tension: 0
+          }
+        },
+        scales: {
+          xAxes: [{
+            type: 'linear',
+            position: 'bottom'
+          }]
+        }
+      }
+    });
   }
 
   render() {
     return (
       <div>
-        <canvas id="chart" ref={this.canvasRef}/>
+        <canvas id="chart" ref={this.canvasRef} />
       </div>
     );
   }
