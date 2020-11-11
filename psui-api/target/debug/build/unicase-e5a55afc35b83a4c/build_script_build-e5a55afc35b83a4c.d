f20c6ee093bc8c0cf6a44a5eb5aa5b1ab796271e
Jay Rixie <jayrixie@gmail.com>
2020-11-11 18:36:16 -0500

    Changed file structure to match Rust's default

---
+++ psui-api/target/debug/build/unicase-e5a55afc35b83a4c/build_script_build-e5a55afc35b83a4c.d
@@ -0,0 +1,5 @@
+/mnt/j/Foundry/PSUI/backend/psui-api/target/debug/build/unicase-e5a55afc35b83a4c/build_script_build-e5a55afc35b83a4c: /home/jrixie/.cargo/registry/src/github.com-1ecc6299db9ec823/unicase-1.4.2/build.rs
+
+/mnt/j/Foundry/PSUI/backend/psui-api/target/debug/build/unicase-e5a55afc35b83a4c/build_script_build-e5a55afc35b83a4c.d: /home/jrixie/.cargo/registry/src/github.com-1ecc6299db9ec823/unicase-1.4.2/build.rs
+
+/home/jrixie/.cargo/registry/src/github.com-1ecc6299db9ec823/unicase-1.4.2/build.rs:
