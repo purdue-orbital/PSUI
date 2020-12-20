e8aed1f8166392d081e42beb06c3d8349dcaf528
MattToast <matthew.drozt@gmail.com>
2020-12-19 19:45:20 -0600

    Generic indicator table is build dynamically

--- gui/src/components/IndicatorTable.js
+++ gui/src/components/IndicatorTable.js
@@ -5,16 +5,15 @@ import GenericIndicator from './Indicators/GenericIndicator.js';
 class IndicatorTable extends React.Component {
   static defaultProps = {
     indicators: {
-      packetsSent: {name: "Packets Sent", data: 0, },
-      packetsRecieved: {name: "Packets Recieved", data: 0, },
-      QDM: {name: "QDM", data: false, },
-      Ignition: {name: "Ignition", data: false, },
-      GSRadio: {name: "GS Radio", data: false, },
-      PlatformStability: {name: "Platform Stability", data: false, },
-      PlatformRadio: {name: "Platform Radio", data: false, },
+      packetsSent: { name: "Packets Sent", data: 0, },
+      packetsRecieved: { name: "Packets Recieved", data: 0, },
+      QDM: { name: "QDM", data: false, },
+      Ignition: { name: "Ignition", data: false, },
+      GSRadio: { name: "GS Radio", data: false, },
+      PlatformStability: { name: "Platform Stability", data: false, },
+      PlatformRadio: { name: "Platform Radio", data: false, },
     },
-    rows: null,
-    cols: null,
+    cols: 3,
   }
 
   constructor(props) {
@@ -22,17 +21,27 @@ class IndicatorTable extends React.Component {
     this.state = {};
   }
 
-  buildTable() {
+  __buildTable() {
     const indicators = this.props.indicators;
-    const inidcaorKeys = Object.keys(indicators);
+    const indicatorKeys = Object.keys(indicators);
+    const cols = this.props.cols;
+    const rows = indicatorKeys.length / cols;
+
+    let indicatorTable = [];
+    for (let i = 0; i < rows; i++) {
+      indicatorTable[i] = indicatorKeys.slice(i * cols, (i + 1) * cols)
+    }
+
+    console.log(indicatorTable);
+
+    const buildRow = (arrayOfKeys) => {
+      return arrayOfKeys.map((key, i) => (<td><GenericIndicator name={indicators[key].name} data={indicators[key].data}/></td>))
+    };
+
     return (
       <tbody>
         {
-          inidcaorKeys.map((key, i) => (
-            <tr>
-              <td><GenericIndicator name={indicators[key].name} data={indicators[key].data}/></td>
-            </tr>
-          ))
+          indicatorTable.map((x, i) => (<tr key={i}>{buildRow(x)}</tr> ))
         }
       </tbody>
     );
@@ -43,7 +52,7 @@ class IndicatorTable extends React.Component {
       <div>
         Put table
         <table>
-          {this.buildTable()}
+          {this.__buildTable()}
         </table>
       </div>
     );
