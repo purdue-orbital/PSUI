5066b4f34e90d1ddca25c7048cf6de5e773ddde7
MattToast <matthew.drozt@gmail.com>
2020-12-19 20:09:02 -0600

    Allows users to pass in thier own layout, creates empty entries on null, generates random layout if none is supplied

--- gui/src/components/IndicatorTable.js
+++ gui/src/components/IndicatorTable.js
@@ -13,35 +13,49 @@ class IndicatorTable extends React.Component {
       PlatformStability: { name: "Platform Stability", data: false, },
       PlatformRadio: { name: "Platform Radio", data: false, },
     },
+    layout: null,
     cols: 3,
   }
 
   constructor(props) {
     super(props);
     this.state = {};
+    this.__layout = this.props.layout != null ? this.props.layout : this.__generateLayout();
+  }
+
+  __generateLayout() {
+    const indicators = this.props.indicators;
+    const indicatorKeys = Object.keys(indicators);
+    const cols = this.props.cols;
+    const rows = Math.ceil(indicatorKeys.length / cols);
+
+    let indicatorTable = [];
+    for (let i = 0; i < rows; i++) {
+      indicatorTable[i] = indicatorKeys.slice(i * cols, (i + 1) * cols);
+    }
+
+    for (let i = indicatorTable[rows - 1].length; i < cols; i++) {
+      indicatorTable[rows - 1].push(null);
+    }
+
+    return indicatorTable;
   }
 
   __buildTable() {
     const indicators = this.props.indicators;
-    const indicatorKeys = Object.keys(indicators);
-    const cols = this.props.cols;
-    const rows = indicatorKeys.length / cols;
-
-    let indicatorTable = [];
-    for (let i = 0; i < rows; i++) {
-      indicatorTable[i] = indicatorKeys.slice(i * cols, (i + 1) * cols)
-    }
-
-    console.log(indicatorTable);
+    const layout = this.__layout;
 
     const buildRow = (arrayOfKeys) => {
-      return arrayOfKeys.map((key, i) => (<td><GenericIndicator name={indicators[key].name} data={indicators[key].data}/></td>))
+      return arrayOfKeys.map((key, i) => {
+        if (key == null) {return (<td></td>)}
+        return (<td><GenericIndicator name={indicators[key].name} data={indicators[key].data} /></td>)
+      });
     };
 
     return (
       <tbody>
         {
-          indicatorTable.map((x, i) => (<tr key={i}>{buildRow(x)}</tr> ))
+          layout.map((x, i) => (<tr key={i}>{buildRow(x)}</tr>))
         }
       </tbody>
     );
