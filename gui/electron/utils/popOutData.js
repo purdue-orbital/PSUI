287713017f5d46da00f1bd230e94a292dd7f08ac
MattToast <matthew.drozt@gmail.com>
2021-02-24 18:41:32 -0500

    Effectively limits there to being one window open per session

--- gui/electron/utils/popOutData.js
+++ gui/electron/utils/popOutData.js
@@ -1,27 +1,53 @@
 const { app, BrowserWindow } = require('electron');
 
 
-function createPopOutData() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 692,
-    minHeight: 590,
-    webPreferences: {
-      nodeIntegration: true
-    },
-  });
 
-  // and load the html for the popout window
-  win.loadURL("https://github.com/purdue-orbital/PSUI/");
+class PopOutDataSingleton {
+  static dataWindow = null;
+
+  static getWindow() {
+    if (PopOutDataSingleton.dataWindow == null) {
+      PopOutDataSingleton.dataWindow = new PopOutData();
+    }
+    PopOutDataSingleton.dataWindow.launchPopOutData();
+  }
+}
+
+class PopOutData {
+
+  __isOpen = false;
+
+  createPopOutData() {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      minWidth: 692,
+      minHeight: 590,
+      webPreferences: {
+        nodeIntegration: true
+      },
+    });
+
+    // and load the html for the popout window
+    win.loadURL("https://github.com/purdue-orbital/PSUI/");
+  }
+
+  launchPopOutData() {
+    if (this.__isOpen) {
+      // TODO: Bring window to front
+    } else {
+      this.__isOpen = true;
+      app.whenReady().then(this.createPopOutData);
+      app.on('window-all-closed', () => { 
+        app.quit();
+      });
+    }
+  }
 }
 
 
-function launchPopOutData() {
-  app.whenReady().then(createPopOutData);
-  app.on('window-all-closed', () => { app.quit() });
-}
+
 
 module.exports = {
-  launchPopOutData: launchPopOutData,
+  launchPopOutData: PopOutDataSingleton.getWindow,
 };
