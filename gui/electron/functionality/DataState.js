56785130172fe964e5fe9b0121e1513ac6be84fa
Roshan Moses Rachapudi <roshan.rachapudi@gmail.com>
2021-04-21 14:32:00 -0700

    added distance calculations

--- gui/electron/functionality/DataState.js
+++ gui/electron/functionality/DataState.js
@@ -35,6 +35,9 @@ class DataState {
     __startRandomData() {
         // Currently gets random data. Probably shouldn't send this to production...
         this.loadDataInterval = setInterval(() => {
+        const lat2 = Math.random(); 
+        const lon2 = Math.random();
+        const dist = this.__calcDistance(lat2, lon2);
             this.currData = {
                 Acceleration: { // Begin Acceleration
                     X: Math.random(),
@@ -46,15 +49,54 @@ class DataState {
                     Y: Math.random(),
                     Z: Math.random(),
                 }, // End Gyro
-                Distance: Math.random(),
+                Distance: dist,
+                /*get Distance() {
+                	__calcDistance(this.Latitude, this.Longitude);
+                },*/
                 Altitude: Math.random(),
                 Temperature: Math.random(),
-                Latitude: Math.random(),
-                Longitude: Math.random(),
+                Latitude: lat2,
+                Longitude: lon2,
+                /*
+                                get Bearing() {
+                	__calcBearing(this.Latitude, this.Longitude);
+               	},
+               	*/
             };
         }, 1000);
     }
-
+    
+    __calcDistance(lat2, lon2) {
+    	// calculates and returns distance in meters from launch site
+    	// DEFINE LON1 AND LAT1 to be launch site coordinates
+    	const lat1 = 0
+    	const lon1 = 0
+    	const R = 6371e3; // metres
+    	const phi1 = lat1 * Math.PI/180; // φ, λ in radians
+			const phi2 = lat2 * Math.PI/180;
+			const dPhi = (lat2-lat1) * Math.PI/180;
+			const dLambda = (lon2-lon1) * Math.PI/180;
+			const a = Math.sin(dPhi/2) * Math.sin(dPhi/2) + Math.cos(phi1) * Math.cos(phi2) * Math.sin(dLambda/2) * Math.sin(dLambda/2);
+			const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+			const d = R * c; // in metres
+			return d;
+    }
+    
+    __calcBearing(lat2, lon2) {
+    	// calculates and returns bearing in degrees
+    	// DEFINE LON1 AND LAT1 to be launch site coordinates
+    	const lat1 = 0
+    	const lon1 = 0
+    	lambda1 = lon1 * Math.PI/180;
+    	lambda2 = lon2 * Math.PI/180;
+    	phi1 = lat1 * Math.PI/180;
+    	phi2 = lat2 * Math.PI/180;
+    	const y = Math.sin(lambda2-lambda1) * Math.cos(phi2);
+    	const x = Math.cos(phi1)*Math.sin(phi2) - Math.sin(phi1)*Math.cos(phi2)*Math.cos(lambda2-lambda1);
+    	const theta = Math.atan2(y,x);
+    	return (theta*180/Math.PI + 360) % 360;
+    }
+    
     getCurrData() {
         return this.currData;
     }
