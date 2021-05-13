356ba9a8fd4d198ab5c8117bd4beccaa8fc09349
MattToast <matthew.drozt@gmail.com>
2021-05-13 14:04:39 -0400

    Updates to the RadioSingleton.py to make comatiple with RadioAlpha2.py

--- radio-api/RadioSingleton.py
+++ radio-api/RadioSingleton.py
@@ -1,9 +1,11 @@
-import RadioBeta as Radio
+import RadioAlpha2 as Radio
 import json
 
+
 class Singleton:
-    __instance = None
-    def __init__(self, DEBUG = 0, isGroundStation = False, hostname = '127.0.0.1'):
+    __instance = None  # FIXME: This is never used, I believe this is supoosed to exisit on RadioSingleton
+
+    def __init__(self, DEBUG=0, isGroundStation=False, hostname='127.0.0.1'):
         if RadioSingleton.__instance is not None:
             raise Exception("Constructor should not be called")
         else:
@@ -15,9 +17,8 @@ class Singleton:
         return RadioSingleton.__instance
 
 
-
 class RadioSingleton:
-    def __init__(self, DEBUG = 0, isGroundStation = False, hostname = '127.0.0.1'):
+    def __init__(self, DEBUG=0, isGroundStation=False, hostname='127.0.0.1'):
         try:
             self.__radio = Radio(DEBUG, isGroundStation, hostname)
         except Exception as e:
@@ -34,10 +35,10 @@ class RadioSingleton:
 
     def getLaunchFlag(self):
         self.__radio.getLaunchFlag()
-    
+
     def getQDMFlag(self):
         self.__radio.getQDMFlag()
-    
+
     def getAbortFlag(self):
         self.__radio.getAbortFlag()
 
