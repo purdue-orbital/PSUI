84c952d083dd24ba22b67677df1afd04f0085f65
Chris Page <page83@purdue.edu>
2021-09-08 19:31:52 -0400

    updated CLI

--- radioapi/radioapi/CLI.py
+++ radioapi/radioapi/CLI.py
@@ -1,17 +1,19 @@
 
 import sys
 import json
+from time import sleep
+from pynput import keyboard
 
 from .GSRadio import GSRadio
 
-def main():
+# old main, not used
+def main_old():
     command = "start"
 
     radio = GSRadio()
     while command!="quit" :
 
-        print("Enter command: ")
-        command = input()
+        command = input("Enter command: ")
         if (command == "A") :
             #test json sending
             radio.send(json.dumps({
@@ -48,6 +50,62 @@ def main():
             #recieve return signal then loop again
         elif command != "quit":
             print("Invalid command.")
+
+def main():
+    radio = GSRadio()
+
+    listener = keyboard.Listener(on_press = lambda x: on_press(x, radio))
+    listener.start()
+
+    print("started")
+    while True:
+        sleep(1)
+
+def on_press(key, radio):
+    if key == keyboard.Key.esc:
+        return False    # stops the listener on escape key
+
+    try:    # assigns the name of the key pressed to variable k
+        k = key.char
+    except:
+        k = key.name
+
+    if k in ["a", "s", "q", "l"]:
+        if k == "a":
+            print("A key pressed")
+            radio.send(json.dumps({
+                "LAUNCH": False,
+                "QDM": False,
+                "ABORT": True,
+                "STAB": False,
+            }))
+
+        elif k == "s":
+            print("S key pressed")
+            radio.send(json.dumps({
+                "LAUNCH": False,
+                "QDM": False,
+                "ABORT": False,
+                "STAB": True,
+            }))
+
+        elif k == "q":
+            print("Q key pressed")
+            radio.send(json.dumps({
+                "LAUNCH": False,
+                "QDM": True,
+                "ABORT": False,
+                "STAB": False,
+            }))
+
+        elif k == "l":
+            print("L key pressed")
+            radio.send(json.dumps({
+                "LAUNCH": True,
+                "QDM": False,
+                "ABORT": False,
+                "STAB": False,
+            }))
 
 if __name__=='__main__':
     main()
