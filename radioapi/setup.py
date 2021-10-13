4e93ec93c0c1bebb1ab24b98ce72e4f0d9aebeb5
Chris Page <page83@purdue.edu>
2021-10-13 18:19:21 -0400

    WIP, Matt has better things for us to do rn

--- radioapi/setup.py
+++ radioapi/setup.py
@@ -1,4 +1,34 @@
-from setuptools import setup, find_packages
+from setuptools import _install_setup_requires, setup, find_packages
+import os
+
+add_packages = find_packages(exclude=(
+    'tests',
+    'json_format',
+    'GNU Radio',
+    'mocks'))
+
+print("add_packages:")
+print(add_packages)
+print()
+
+libFolder = os.path.dirname(os.path.realpath(__file__))
+requirementsPath = libFolder + "/requirements.txt"
+
+install_requirements=[]
+if os.path.isfile(requirementsPath):
+    with open(requirementsPath) as f:
+        install_requirements = f.read().splitlines()
+
+print("Requirements:")
+print(install_requirements)
+print()
+
+for package in install_requirements:
+    add_packages.append(package)
+
+print("All packages:")
+print(add_packages)
+print()
 
 setup(
     name='radioapi',
@@ -9,11 +39,5 @@ setup(
     author_email='matthew.drozt@gmail.com',
     url='https://github.com/purdue-orbital/PSUI',
     license='TODO',
-    packages=find_packages(exclude=(
-            'tests',
-            'json_format',
-            'GNU Radio',
-            'mocks'
-        )
-    )
+    packages=add_packages
 )
