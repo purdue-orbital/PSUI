aabefc91d5ce1c55eb378f519b82c976ab587daf
MattToast <matthew.drozt@gmail.com>
2021-11-29 20:27:47 -0500

    read from serial

--- radioapi/radioapi/serialcoms/serialcoms.py
+++ radioapi/radioapi/serialcoms/serialcoms.py
@@ -33,26 +33,41 @@ class SerialComs:
     def __init__(self, port: str, baudrate: int) -> None:
         self.ser = serial.Serial(port, baudrate)
 
-    def recieve_forever(self, func: Callable[[str], None]) -> None:
+    def recieve_forever(self, func: Callable[[SerialComMessage], None]) -> None:
+        msg = ''
         while True:
-            func(self.ser.read_until())
+            char = self.ser.read().decode()
+            if char == '&':
+                func(SerialComMessage.from_string(msg))
+                msg = ''
+            else:
+                msg += char
 
     def write(self, msg: SerialComMessage):
         self.ser.write(f"{json.dumps(msg.as_dict)}\n".encode())
         self.ser.flush()
-        
 
 
 if __name__ == '__main__':
-    d = SerialComMessage.from_string('{"ABORT": 1, "QDM": 1, "STAB": 1, "LAUNCH": 1, "ARM": 0}')
+    # d = SerialComMessage.from_string('{"ABORT": 1, "QDM": 1, "STAB": 1, "LAUNCH": 1, "ARM": 0}')
 
-    print(d)
-    print(d.ARM)
-    print(d['ARM'])
-    print(d)
+    # print(d)
+    # print(d.ARM)
+    # print(d['ARM'])
+    # print(d)
 
-    print(type('mystr'.encode()))
+    # print(type('mystr'.encode()))
 
-    # c = SerialComs('/dev/ttyUSB0', 9600)
-    # c.recieve_forever(lambda x: print(f'Message: {x}'))
+    print('yo')
+    c = SerialComs('/dev/ttyUSB0', 9600)
+    c.recieve_forever(lambda x: print(f'Message: {x}'))
 
+    # ser = serial.Serial('/dev/ttyUSB0', 9600)
+    # # ser.write(b"FUCK RADIOS")
+    # # ser.flush()
+    # # print('done')
+
+    # # .decode("utf-8")
+    # while True:
+    #     print(f'Message -> {ser.read_until(expected="&")}')
+    #     print('YOO')
