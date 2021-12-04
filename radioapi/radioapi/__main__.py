108406d6fb30296dae5a81a2704e3c422711af53
MattToast <matthew.drozt@gmail.com>
2021-12-03 22:58:11 -0500

    default port

--- radioapi/radioapi/__main__.py
+++ radioapi/radioapi/__main__.py
@@ -6,7 +6,7 @@ def main():
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
-    port = args[0]
+    port = args[0] if args else "/dev/ttyUSB0"
     cli.main(port)
 
 
