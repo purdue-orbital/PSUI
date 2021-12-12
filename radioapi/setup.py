dca28fd5f9dfb69f87f55bbfca2bb337d844e3ef
MattToast <matthew.drozt@gmail.com>
2021-12-11 21:28:13 -0500

    add package installation files

--- radioapi/setup.py
+++ radioapi/setup.py
@@ -1,19 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='radioapi',
-    version='0.0.2',
-    description='Purdue Orbital Radio API',
-    long_description='TODO',
-    author='Matt Drozt',
-    author_email='matthew.drozt@gmail.com',
-    url='https://github.com/purdue-orbital/PSUI',
-    license='TODO',
-    packages=find_packages(exclude=(
-            'tests',
-            'json_format',
-            'GNU Radio',
-            'mocks'
-        )
-    )
-)
+
+if __name__ == "__main__":
+    setup()
